4d45b9caaebc1f4879e7a812bfecb935bed9d6ba
vandaleng <vanda.leng@student.passerellesnumeriques.org>
2025-03-07 20:38:54 +0700

    feat(add user): added notification want to see when the user order drink[jira-14]

---
+++ views/partials/notification_panel.php
@@ -0,0 +1,11 @@
+<div class="notification-panel" id="notificationPanel">
+    <button class="close-btn" onclick="toggleNotificationPanel()">×</button>
+    <h3>Notifications</h3>
+    <div class="notification-list" id="notificationList">
+        <div class="empty-notification">
+            <i class="fas fa-bell-slash"></i>
+            <p>No notifications yet</p>
+        </div>
+    </div>
+</div>
+
