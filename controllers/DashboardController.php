c243994b595e7b9a5d9da99e4f5703ddf180c197
vandaleng <vanda.leng@student.passerellesnumeriques.org>
2025-03-16 14:00:29 +0700

    fix(add user): fix feature/dashboard [jira-16]

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -1,9 +1,10 @@
 <?php
-class DashboardController extends BaseController {
-    public function index() {
-        // Add any dashboard data you need here
-        $this->view('dashboard', [
-            'title' => 'Dashboard'
-        ]);
+require_once 'BaseController.php';
+class DashboardController extends BaseController{
+    function index()
+    {
+        
+        $this->views('dashboard/list.php');
     }
 }
+?>
