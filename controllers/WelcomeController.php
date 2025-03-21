9b5640288ab9c555e795e32101f014fe142c74e6
Sreyneath ROM <sreyneath.rom@student.passerellnumeriques.org>
2025-03-21 09:31:46 +0700

    Resolved conflicts in OrdersController, WelcomeController, and Views

--- controllers/WelcomeController.php
+++ controllers/WelcomeController.php
@@ -1,7 +1,21 @@
 <?php
-
 class WelcomeController extends BaseController {
     public function welcome() {
-        $this->views('welcome/welcome');
+        // Check if user is logged in
+        $isLoggedIn = isset($_SESSION['user_id']);
+        
+        // Pass data to the view
+        $data = [
+            'title' => 'Welcome to XING FU CHA',
+            'isLoggedIn' => $isLoggedIn
+        ];
+        
+        // If user is logged in, add user data
+        if ($isLoggedIn && isset($_SESSION['user'])) {
+            $data['user'] = $_SESSION['user'];
+        }
+        
+        $this->views('welcome/welcome', $data);
     }
 }
+
