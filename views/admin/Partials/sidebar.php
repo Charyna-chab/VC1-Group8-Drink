4403cc6a94d7d23c0c2000679e00f1c2a7478f06
charyna-chab <charyna.chab@student.passerellesnumeriques.org>
2025-04-02 08:29:14 +0700

    feat(feature/connect-product):change interface dashboard[jira-62]

--- views/admin/Partials/sidebar.php
+++ views/admin/Partials/sidebar.php
@@ -1,11 +1,11 @@
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav bg-white sidebar sidebar-light accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/admin-dashboard">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
         <div class="sidebar-brand-icon rotate-n-15">
-            <img src="/assets/image/logo/logo.png" alt="XING FU CHA" style="width: 50px;">
+            <img src="/placeholder.svg" alt="">
         </div>
-        <div class="sidebar-brand-text mx-3">XING FU CHA</div>
+        <div class="sidebar-brand-text mx-3 text-dark">XING FU CHA</div>
     </a>
 
     <!-- Divider -->
@@ -13,98 +13,60 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item active">
-        <a class="nav-link" href="/admin-dashboard">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span>
-        </a>
+        <a class="nav-link" href="/">
+            <i class="fas fa-fw fa-tachometer-alt text-dark"></i>
+            <span>Dashboard</span></a>
     </li>
 
     <!-- Divider -->
     <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Management
-    </div>
-
-    <!-- Nav Item - Products -->
+    <!-- Nav Item - Utilities Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link" href="/admin/products">
-            <i class="fas fa-fw fa-coffee"></i>
-            <span>Products</span>
-        </a>
-    </li>
-
-    <!-- Nav Item - Users -->
-    <li class="nav-item">
-        <a class="nav-link" href="/admin/users">
-            <i class="fas fa-fw fa-users"></i>
+        <a class="nav-link collapsed" href="/views/user/list.php"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <i class="fa fa-user" aria-hidden="true"></i>
             <span>Customers</span>
         </a>
     </li>
 
-    <!-- Nav Item - Orders -->
-    <li class="nav-item">
-        <a class="nav-link" href="/admin/orders">
-            <i class="fas fa-fw fa-shopping-cart"></i>
-            <span>Orders</span>
-        </a>
-    </li>
-
     <!-- Divider -->
     <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Feedback
-    </div>
-
-    <!-- Nav Item - Feedback -->
+    <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link" href="/admin/feedback">
-            <i class="fas fa-fw fa-comment"></i>
-            <span>Feedback</span>
+        <a class="nav-link collapsed" href="/product">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Products</span>
         </a>
     </li>
-
-    <!-- Divider -->
     <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Settings
-    </div>
-
-    <!-- Nav Item - Settings -->
+    <!-- Nav Item - Charts -->
     <li class="nav-item">
-        <a class="nav-link" href="/admin/settings">
-            <i class="fas fa-fw fa-cog"></i>
-            <span>Settings</span>
-        </a>
+        <a class="nav-link" href="/feedback">
+            <i class="fas fa-comment-alt feedback-icon"></i>
+            <span>Feedback</span></a>
     </li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
-
-    <!-- Nav Item - Return to User View -->
+    <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link" href="/order">
-            <i class="fas fa-fw fa-home"></i>
-            <span>Return to Shop</span>
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+            aria-expanded="true" aria-controls="collapsePages">
+            <i class="fas fa-cogs setting-icon"></i>
+            <span>Sitting</span>
         </a>
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Login Screens:</h6>
+                <a class="collapse-item" href="login.html">Login</a>
+                <a class="collapse-item" href="register.html">Register</a>
+                <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
+                <div class="collapse-divider"></div>
+                <h6 class="collapse-header">Other Pages:</h6>
+                <a class="collapse-item" href="404.html">404 Page</a>
+                <a class="collapse-item" href="blank.html">Blank Page</a>
+            </div>
+        </div>
     </li>
-
-    <!-- Nav Item - Logout -->
-    <li class="nav-item">
-        <a class="nav-link" href="#" data-toggle="modal" data-target="#logoutModal">
-            <i class="fas fa-fw fa-sign-out-alt"></i>
-            <span>Logout</span>
-        </a>
-    </li>
-
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
 </ul>
-
