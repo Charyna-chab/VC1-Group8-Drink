469fc17fe531330101c07088c48fd4422b5f0219
charyna-chab <charyna.chab@student.passerellesnumeriques.org>
2025-04-02 09:30:25 +0700

    feat(feature/connect-product): added option logout to dashboard[jira-64

--- views/admin/Partials/sidebar.php
+++ views/admin/Partials/sidebar.php
@@ -58,14 +58,9 @@
         </a>
         <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Login Screens:</h6>
-                <a class="collapse-item" href="login.html">Login</a>
-                <a class="collapse-item" href="register.html">Register</a>
-                <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                <div class="collapse-divider"></div>
-                <h6 class="collapse-header">Other Pages:</h6>
-                <a class="collapse-item" href="404.html">404 Page</a>
-                <a class="collapse-item" href="blank.html">Blank Page</a>
+
+                <a href="/logout" class="role-switch-btn"><i class="fas fa-sign-out-alt"></i> Logout</a>
+
             </div>
         </div>
     </li>
