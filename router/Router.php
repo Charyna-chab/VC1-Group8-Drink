4102aff1f76ccbd0d3a54653b0524c2a22a89a42
vandaleng <vanda.leng@student.passerellesnumeriques.org>
2025-04-07 22:58:21 +0700

    fix(feature/Main): fix issue feature/router file page [jira-00]

--- router/Router.php
+++ router/Router.php
@@ -3,15 +3,15 @@ namespace YourNamespace;
 
 class Router {
     private $routes = [];
-
+    
     public function get($uri, $controller) {
         $this->routes['GET'][$uri] = $controller;
     }
-
+    
     public function post($uri, $controller) {
         $this->routes['POST'][$uri] = $controller;
     }
-
+    
     public function route() {
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         $method = $_SERVER['REQUEST_METHOD'];
@@ -35,24 +35,15 @@ class Router {
                 return;
             }
         }
-
+        
         // Check for exact route match
         if (isset($this->routes[$method][$uri])) {
             list($controllerClass, $method) = $this->routes[$method][$uri];
-            
-            // Check if the controller class already includes the namespace
-            if (strpos($controllerClass, '\\') === false) {
-                $controllerClass = '\\YourNamespace\\Controllers\\' . $controllerClass;
-            }
-
-            if (!class_exists($controllerClass)) {
-                throw new \Exception("Controller class $controllerClass not found. Check the namespace and file path.");
-            }
             $controllerInstance = new $controllerClass();
             $controllerInstance->$method();
             return;
         }
-
+        
         // Route not found
         header('HTTP/1.1 404 Not Found');
         echo '404 Page Not Found';
