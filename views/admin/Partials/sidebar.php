7245c30fb3eaa6fde41824bac8d749d9b92e7c3f
vandaleng <vanda.leng@student.passerellesnumeriques.org>
2025-04-08 13:10:13 +0700

    fix(feature/sidebar): fix issue feature/sidebar style active page [jira-00]

--- views/admin/Partials/sidebar.php
+++ views/admin/Partials/sidebar.php
@@ -11,17 +11,6 @@ $currentPage = basename($_SERVER['REQUEST_URI']);
         </div>
         <div class="sidebar-brand-text mx-3 text-dark">XING FU CHA</div>
     </a>
-<<<<<<< HEAD
-    
-    <!-- Divider -->
-    <hr class="sidebar-divider my-0">
-
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item active" style="background: #ff5e62">
-        <a class="nav-link" href="/admin-dashboard" style="color: white !important;">
-            <i class="fas fa-fw fa-tachometer-alt" style="color: white !important;"></i>
-            <span style="color: white !important;">Dashboard</span>
-=======
 
     <hr class="sidebar-divider my-0">
 
@@ -30,22 +19,15 @@ $currentPage = basename($_SERVER['REQUEST_URI']);
         <a class="nav-link" href="/admin-dashboard">
             <i class="fas fa-fw fa-tachometer-alt text-dark"></i>
             <span>Dashboard</span>
->>>>>>> fe0c7aa96ac9744f359c6247f37e9be1060f6d1c
         </a>
     </li>
 
     <hr class="sidebar-divider">
 
     <!-- Nav Item - Customers -->
-<<<<<<< HEAD
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="/admin/users">
-            <i class="fa fa-user" aria-hidden="true" style="color: black !important;"></i>
-=======
     <li class="nav-item <?= $currentPage == 'users' ? 'active-page' : '' ?>">
         <a class="nav-link" href="/admin/users">
             <i class="fa fa-user" aria-hidden="true"></i>
->>>>>>> fe0c7aa96ac9744f359c6247f37e9be1060f6d1c
             <span>Customers</span>
         </a>
     </li>
@@ -53,16 +35,9 @@ $currentPage = basename($_SERVER['REQUEST_URI']);
     <hr class="sidebar-divider">
 
     <!-- Nav Item - Products -->
-<<<<<<< HEAD
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-            aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-folder" style="color: black !important;"></i>
-=======
     <li class="nav-item <?= $currentPage == 'product' ? 'active-page' : '' ?>">
         <a class="nav-link" href="/product">
             <i class="fas fa-fw fa-folder"></i>
->>>>>>> fe0c7aa96ac9744f359c6247f37e9be1060f6d1c
             <span>Products</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
@@ -83,15 +58,9 @@ $currentPage = basename($_SERVER['REQUEST_URI']);
     <hr class="sidebar-divider">
 
     <!-- Nav Item - Receipts -->
-<<<<<<< HEAD
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="/receipt">
-            <i class="fas fa-receipt" style="color: black !important;"></i>
-=======
     <li class="nav-item <?= $currentPage == 'receipt' ? 'active-page' : '' ?>">
         <a class="nav-link" href="/receipt">
             <i class="fas fa-receipt"></i>
->>>>>>> fe0c7aa96ac9744f359c6247f37e9be1060f6d1c
             <span>Receipts</span>
         </a>
     </li>
@@ -101,14 +70,9 @@ $currentPage = basename($_SERVER['REQUEST_URI']);
     <!-- Nav Item - Feedback -->
     <li class="nav-item <?= $currentPage == 'feedback' ? 'active-page' : '' ?>">
         <a class="nav-link" href="/admin/feedback">
-<<<<<<< HEAD
-            <i class="fas fa-comment-alt feedback-icon" style="color: black !important;"></i>
-            <span>Feedback</span></a>
-=======
             <i class="fas fa-comment-alt feedback-icon"></i>
             <span>Feedback</span>
         </a>
->>>>>>> fe0c7aa96ac9744f359c6247f37e9be1060f6d1c
     </li>
 
     <!-- Nav Item - Order List -->
