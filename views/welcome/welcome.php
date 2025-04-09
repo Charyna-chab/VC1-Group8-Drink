8fe0270e47ad1fe8f01e0f87f963ac02ca605572
Sreyneath-Rom <sreyneath.rom@student.passerellesnumeriques.org>
2025-04-09 10:37:09 +0700

    fix: add missing line-clamp property for improved text truncation in navigation

--- views/welcome/welcome.php
+++ views/welcome/welcome.php
@@ -586,6 +586,7 @@ nav ul li a:hover {
     margin: 0 auto 10px;
     display: -webkit-box;
     -webkit-line-clamp: 2;
+    line-clamp: 2;
     -webkit-box-orient: vertical;
     overflow: hidden;
     text-overflow: ellipsis;
